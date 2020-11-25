ef1519ddaaced2ec5a3427d555bbdc9563685f10
jennifernatis <jennifer.natis@gmail.com>
2020-11-25 18:34:09 -0500

    guardando lo del 24

--- src/estudio/ejercitando/TablaDeVerdad2.java
+++ src/estudio/ejercitando/TablaDeVerdad2.java
@@ -4,6 +4,10 @@ public class TablaDeVerdad2 {
 
 public static void main (String [] args)
 {
+//  System.out.println(true + " AND " + true +" = "+ (true&&true) ); 
+  System.out.println(" true AND true = "+ (true&&true) );
+  System.out.println(" true AND false = "+ (true&&false) ); 
+	
 System.out.println("cualquier otra tabla de verdad");
 System.out.println("quiero hacer algo.. ");
 }
